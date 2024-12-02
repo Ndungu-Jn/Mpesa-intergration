f9e47b6dcffeb46a4f67a60ac5c4a7a3b2301c56
Ndungu-Jn <ndungulink@gmail.com>
2024-12-02 09:09:50 +0300

    Mpesa integration

---
+++ main_app/views.py
@@ -0,0 +1,25 @@
+import json
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django_daraja.mpesa.core import MpesaClient
+
+
+# Create your views here.
+def trigger(request):
+    cl = MpesaClient()
+    phone_number = '0718818167'
+    amount = 1
+    account_reference = '001-XYZ'
+    transaction_desc = 'madeni'
+    callback_url = 'https://pleasing-partly-hamster.ngrok-free.app/callback'
+    response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
+    return HttpResponse(response)
+
+
+@csrf_exempt
+def callback(request):
+    resp = json.loads(request.body)
+    print(resp)
+    return HttpResponse("OK")
